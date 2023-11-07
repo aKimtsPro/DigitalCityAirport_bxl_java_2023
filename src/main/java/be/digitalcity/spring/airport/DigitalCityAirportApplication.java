ddab6788c280799656fce9a1e220ec27c93ad4e1
Alexandre Kimtsaris <alexandre.kimtsaris@bstorm.be>
2023-11-07 10:48:48 +0100

    swagger security

--- src/main/java/be/digitalcity/spring/airport/DigitalCityAirportApplication.java
+++ src/main/java/be/digitalcity/spring/airport/DigitalCityAirportApplication.java
@@ -2,6 +2,7 @@ package be.digitalcity.spring.airport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Scanner;
